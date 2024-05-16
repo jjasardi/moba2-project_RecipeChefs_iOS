650e113f5476012666760ce7861da222f8a11c52
yohan <unveryoh@students.zhaw.ch>
2024-05-16 10:46:09 +0200

    Initial Commit

---
+++ Recipe_Chefs_iOS/Recipe_Chefs_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_Chefs_iOSApp.swift
+//  Recipe_Chefs_iOS
+//
+//  Created by Yohan on 16.05.24.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_Chefs_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
